b0ff9e53121ab4e00ffadbdf58b755177befb44e
Armando Myftari <armandomyftarii@gmail.com>
2024-03-07 01:35:42 +0100

    Added logout button

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,10 @@
+import { LogOutIcon } from 'lucide-react';
 import React, { useState } from 'react';
 import { FiSun, FiMoon } from 'react-icons/fi';
+import { useUserContext } from './UserContext';
 
 const Header: React.FC = () => {
+    const { user, logout } = useUserContext();
     const [darkMode, setDarkMode] = useState(false);
 
     const toggleDarkMode = () => {
@@ -12,12 +15,17 @@ const Header: React.FC = () => {
     return (
         <div className="h-16 border-b w-full flex items-center justify-between bg-purple-700 dark:bg-gray-950 text-white dark:text-white p-4">
             <h1 className="text-lg font-bold cursor-default"> A.M Invoicing</h1>
-            <button
-                onClick={toggleDarkMode}
-                title={darkMode ? 'Toogle Light Mode' : 'Toogle Dark Mode'}
-                className="bg-transparent focus:outline-none border-none hover:border-none hover:scale-125 transition-all">
-                {darkMode ? <FiSun /> : <FiMoon />}
-            </button>
+            <div className='flex items-center gap-0.5 mr-1'>
+                <button
+                    onClick={toggleDarkMode}
+                    title={darkMode ? 'Toogle Light Mode' : 'Toogle Dark Mode'}
+                    className="bg-transparent focus:outline-none border-none hover:border-none hover:scale-150 transition-all duration-300">
+                    {darkMode ? <FiSun /> : <FiMoon />}
+                </button>
+                {user && <button onClick={logout} title='Log Out' className="bg-transparent focus:outline-none border-none hover:border-none hover:scale-150 duration-300 transition-all">
+                    <LogOutIcon className=' size-1 '></LogOutIcon>
+                </button>}
+            </div>
         </div>
     );
 };
